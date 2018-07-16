a77bd310f2e5fd566e36ff2989c60b183a29f17c
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2018-07-16 17:11:42 +0100

    check for mandatory settings

--- rogo-compose.sh
+++ rogo-compose.sh
@@ -1,3 +1,15 @@
+if [ ! -d "$ROGO_DOCKER_WWWROOT" ];
+then
+    echo 'Error: $ROGO_DOCKER_WWWROOT is not set or not an existing directory'
+    exit 1
+fi
+
+if [ -z "$ROGO_DOCKER_MYSQLROOT" ];
+then
+    echo 'Error: $ROGO_DOCKER_MYSQLROOT is not set'
+    exit 1
+fi
+
 dockercompose="docker-compose -f docker-compose.yml"
 
 if [ -z "$ROGO_DOCKER_EXPOSE" ];
